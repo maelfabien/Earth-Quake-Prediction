358e0435d96e910b281f87e71df2d04e6743d3f3
jakaub <jacky.kaub@telecom-paristech.fr>
2019-02-23 14:07:34 +0100

    Adding feature function Adding first ML algorithm Adding Prediction function

--- Sampling.py
+++ Sampling.py
@@ -6,6 +6,13 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 import random
 import os
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestRegressor
+from lightgbm import LGBMRegressor
+from xgboost import XGBRegressor
+from sklearn.metrics import mean_absolute_error
+import glob, os
+
 # Input data files are available in the "../input/" directory.
 # For example, running this (by clicking run or pressing Shift+Enter) will list the files in the input directory
 
@@ -17,6 +24,8 @@ events = np.array([  5656574,  50085878, 104677356, 138772453, 187641820, 218652
       245829585, 307838917, 338276287, 375377848, 419368880, 461811623,
       495800225, 528777115, 585568144, 621985673])
 
+df = pd.read_csv('../input/train.csv', dtype={'acoustic_data': np.int16,'time_to_failure': np.float32}).values
+        
 def gen_index(seg_len):
     """This function generate a list of initial value for the splitting of the dataset"""
     
@@ -83,6 +92,88 @@ def sub_table(index, seg_len):
         usecols=['time_to_failure'], skiprows =range(1, index-1+seg_len), nrows=1).values[0][0]
         
     return (X,y)
+
+def mk_features(X):
     
+    """Creation of features for ML algorithms"""
+    
+    feats = {}
+    feats['mean'] = [X.mean()]
+    feats['std'] = [X.std()]
+    feats['max'] = [X.max()]
+    feats['min'] = [X.min()]
+    
+    feats['mean_change_abs'] = [np.diff(X, axis=0).mean()]
+    feats['mean_change_rate'] = [np.nonzero(np.diff(X, axis=0) / (X[:-1]+0.00000001))[0].mean()]
+    feats['abs_max'] = [np.abs(X).max()]
+    feats['abs_min'] = [np.abs(X).min()]
+    
+    for j in [50000,10000,5000]:
+        for i in range(int(len(X)/j)):
+            X_ = X[i*j:(i+1)*j]
+            feats[f'std_{j}_{i}'] = [X_.std()]
+            feats[f'mean_{j}_{i}'] = [X_.mean()]
+            feats[f'max_{j}_{i}'] = [X_.max()]
+            feats[f'min_{j}_{i}'] = [X_.min()]
+            feats[f'mean_change_rate_{j}_{i}'] = [np.nonzero(np.diff(X_, axis=0) / (X_[:-1]+0.00000001))[0].mean()]
+    
+    feats['max_to_min'] = [X.max()/np.abs(X.min())]
+    feats['max_to_min_diff'] = [X.max() - np.abs(X.min())]
+    
+    for i in  [10,20,50,100,300,500]:
+        feats[f'count_{i}'] = [len((X[np.where(np.abs(X)>i)]))]
+        
+    feats['sum'] = [X.sum()]
+    
+    for q in [0.6,0.65,0.7,0.75,0.8,0.85,0.9,0.95,0.99,0.999]:
+        feats[f'quantile_{q}'] = [np.quantile(X,q)]
+        
+    return pd.DataFrame(feats)
+
+
+import time
 ind= gen_index(150000)
-(X,y) = sub_table(ind[0],150000)
+X=[]
+Y=[]
+j=0
+for i in ind :
+    t1 = time.time()
+    X.append(mk_features(df[i:i+150000,0]))
+    Y.append(df[i+1,1])
+    t2 = time.time()
+    print(f'{j}_sub_table : {round(t2-t1,1)}')
+    j+=1
+
+train = pd.concat(X)
+train['y'] = Y
+
+"""Xtrain, Xtest, ytrain, ytest = train_test_split(train.drop('y',axis=1), train['y'], train_size=0.9)
+
+for n in [0.01,0.05,0.1,0.5,1]:
+    print('**********'+str(n)+'*************')
+    for lamb in [0.001,0.003,0.005,0.1,0.20]:
+        model = XGBRegressor(eta=n, max_depth = 5, reg_lambda = lamb)
+        model.fit(Xtrain,ytrain)
+        print(f"{lamb} : {round(mean_absolute_error(model.predict(Xtest),ytest),2)}")"""
+
+
+#Final model
+model = XGBRegressor(eta=0.05, max_depth = 5, reg_lambda = 0.005).fit(train.drop('y',axis=1),train['y'])
+
+test = []
+#We load all test sets in a list
+for file in glob.glob("../input/test/*.csv"):
+    test.append(file.split('/')[-1].split('.')[0])
+
+Test = []
+for csv in test:
+    Test.append(mk_features(pd.read_csv(f'../input/test/{csv}.csv').values))
+    print(str(round(len(Test)/len(test)*100)) + ' %')
+    
+Xtest = pd.concat(Test)
+
+pred = model.predict(Xtest)
+submit = pd.DataFrame(test)
+submit = submit.rename(columns={0:'seg_id'})
+submit['time_to_failure'] = pred
+submit.to_csv('submission.csv', index=False)
